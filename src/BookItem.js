a558584614b21dcc0540ba27812fabc400dd917f
riccio85 <rihanna.ke@gmail.com>
2018-07-23 22:19:49 +0200

    not found thumbnail image fixed

--- src/BookItem.js
+++ src/BookItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import ShelfChanger from './ShelfChanger'
+import CoverImage from './img/cover404.png';
 
 class BookItem extends Component{
   static propTypes = {
@@ -12,7 +13,11 @@ class BookItem extends Component{
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover"
+            style={{ width: 128, height: 193,
+              backgroundImage:  (book.imageLinks && book.imageLinks.thumbnail) ?
+                      `url(${book.imageLinks.thumbnail})` : `url(${CoverImage})`
+             }}></div>
           <ShelfChanger
                           book={book}
                           changeShelf={this.props.changeShelf}/>
